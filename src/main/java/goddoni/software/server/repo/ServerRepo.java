202da356be065949fe2e9cc00b851ec70d6cbf9c
severingoddon <severin.goddon@gmail.com>
2023-09-03 00:32:02 +0200

    add things proberly

--- src/main/java/goddoni/software/server/repo/ServerRepo.java
+++ src/main/java/goddoni/software/server/repo/ServerRepo.java
@@ -1,2 +1,8 @@
-package goddoni.software.server.repo;public interface ServerRepo {
+package goddoni.software.server.repo;
+
+import goddoni.software.server.model.Server;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ServerRepo extends JpaRepository<Server, Long> {
+    Server findByIpAddress(String ipAddress);
 }
--- src/main/java/goddoni/software/server/service/ServerService.java
+++ src/main/java/goddoni/software/server/service/ServerService.java
@@ -2,10 +2,13 @@ package goddoni.software.server.service;
 
 import goddoni.software.server.model.Server;
 
+import java.io.IOException;
+import java.net.UnknownHostException;
 import java.util.Collection;
 
 public interface ServerService {
     Server create(Server server);
+    Server ping(String ipAddress) throws IOException;
     Collection<Server> list(int limit);
     Server get(Long id);
     Server update(Server server);
