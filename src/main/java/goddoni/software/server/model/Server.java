2fbf8e21202eba6c194b7394824f83d4d96f1069
severingoddon <severin.goddon@gmail.com>
2023-09-02 21:14:21 +0200

    finished server class

--- src/main/java/goddoni/software/server/model/Server.java
+++ src/main/java/goddoni/software/server/model/Server.java
@@ -1,10 +1,24 @@
 package goddoni.software.server.model;
 
 import goddoni.software.server.enumeration.Status;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Server {
+
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
-    private String ipAdress;
+
+    @Column(unique = true)
+    @NotEmpty(message = "IP address cannot be empty or null")
+    private String ipAddress;
     private String name;
     private String memory;
     private String type;
