67253d4cf45f0d24a34c95e98a46b74d7df53e69
severingoddon <severin.goddon@gmail.com>
2023-09-02 21:02:13 +0200

    initial commit

---
+++ src/main/java/goddoni/software/server/model/Server.java
@@ -0,0 +1,13 @@
+package goddoni.software.server.model;
+
+import goddoni.software.server.enumeration.Status;
+
+public class Server {
+    private Long id;
+    private String ipAdress;
+    private String name;
+    private String memory;
+    private String type;
+    private String imageUrl;
+    private Status status;
+}
